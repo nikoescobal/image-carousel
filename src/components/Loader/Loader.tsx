90db50e4cba99d14a6c9f89be45a412cd2dcfbfc
nikoescobal <niko.escobal@gmail.com>
2024-08-15 17:05:10 +0800

    Add Loader

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,28 @@
+import styles from './loader.module.scss';
+
+function Loader() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
